b4563d13bc75394d73ce624d4bcd062f31d3e61c
pkreppel <pkreppel@gmail.com>
2021-06-13 17:55:22 -0300

    RestTemplate

--- hr-payroll/src/main/java/com/pkreppel/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/pkreppel/hrpayroll/services/PaymentService.java
@@ -1,12 +1,27 @@
 package com.pkreppel.hrpayroll.services;
 
 import com.pkreppel.hrpayroll.entities.Payment;
+import com.pkreppel.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
+    @Value("${hr-worker.host}")
+    private String workerHost;
+    @Autowired
+    private RestTemplate restTemplate;
 
     public Payment getPayment(long workerId, int days){
-        return new Payment("Bob", 200.00, days);
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", (""+workerId));
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
