d8985e802221a5afb4939bf1c045b3e7978715d4
pkreppel <pkreppel@gmail.com>
2021-06-19 14:04:06 -0300

    User feign client

---
+++ hr-oauth/src/main/java/com/pkreppel/hroauth/services/UserService.java
@@ -0,0 +1,26 @@
+package com.pkreppel.hroauth.services;
+
+import com.netflix.discovery.converters.Auto;
+import com.pkreppel.hroauth.entities.User;
+import com.pkreppel.hroauth.feignclients.UserFeignClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private static Logger logger = LoggerFactory.getLogger(UserService.class);
+    @Autowired
+    private UserFeignClient userFeignClient;
+    public User findByEmail(String email){
+        User user = userFeignClient.findByEmail(email).getBody();
+        if (user == null){
+            logger.error("Email not found: " + email);
+            throw new IllegalArgumentException("Email not found");
+        }
+        logger.info("Email found: " + email);
+        return user;
+    }
+}
